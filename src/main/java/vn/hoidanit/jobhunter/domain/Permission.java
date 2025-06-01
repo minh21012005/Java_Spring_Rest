aad57c9bb9a99cea4119f3d7f9a5ea15020abfd0
minh21012005 <baminhss@gmail.com>
2025-06-02 01:24:37 +0700

    115

--- src/main/java/vn/hoidanit/jobhunter/domain/Permission.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Permission.java
@@ -14,6 +14,7 @@ import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
@@ -21,6 +22,7 @@ import vn.hoidanit.jobhunter.util.SecurityUtil;
 @Table(name = "permissions")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -63,5 +65,12 @@ public class Permission {
                 : "";
 
         this.updatedAt = Instant.now();
+    }
+
+    public Permission(String name, String apiPath, String method, String module){
+        this.name=name;
+        this.apiPath=apiPath;
+        this.method=method;
+        this.module=module;
     }
 }
